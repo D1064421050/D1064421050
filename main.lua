f56556d338223886f9f8f2afe47981abd47e78d7
D1064421050 <d1064421050@gm.lhu.edu.tw>
2018-10-30 09:24:38 +0800

    1006
    OK

--- main.lua
+++ main.lua
@@ -13,3 +13,6 @@ msgText:setFillColor( 0.2, 0.6, 0.8 )
 
 local msgText = display.newText( "Hello, world!", world.x, world.y+190, native.systemFont, 32 )
 msgText:setFillColor( 0.5, 0.5, 0.1)
+
+local msgText = display.newText( "Hello, world!", world.x, world.y+220, native.systemFont, 32 )
+msgText:setFillColor( 0.1, 0.3, 0.9)
